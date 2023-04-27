1852a3ced812e8804b7bc1f56a59b672a3e052cb
SnappsiSnappes <111605401+SnappsiSnappes@users.noreply.github.com>
2023-04-27 20:53:02 +0300

    Add files via upload

--- gui-test.py
+++ gui-test.py
@@ -4,13 +4,13 @@ import sqlite3
 # Подключение к базе данных SQLite
 conn = sqlite3.connect('mydatabase.db')
 c = conn.cursor()
-c.execute('CREATE TABLE IF NOT EXISTS mytable (id INTEGER PRIMARY KEY, action TEXT, voice TEXT, data TEXT)')
+c.execute('CREATE TABLE IF NOT EXISTS mytable (id INTEGER PRIMARY KEY, name TEXT, action TEXT, voice TEXT, data TEXT)')
 
 # Функция для обновления списка элементов
 def update_listbox():
     listbox.delete(0, tk.END)
     for row in c.execute('SELECT * FROM mytable'):
-        listbox.insert(tk.END, f"{row[0]}: {row[1]} - {row[2]} - {row[3]}")
+        listbox.insert(tk.END, f"{row[0]}: {row[1]} - {row[2]} - {row[3]} - {row[4]}")
 
 # Функция для удаления выбранного элемента
 def delete_item():
@@ -24,10 +24,11 @@ def delete_item():
 # Функция для добавления нового элемента
 def add_item():
     action = action_var.get()
+    name = name_entry.get()
     voice = voice_entry.get()
     data = data_entry.get()
     if action and voice and data:
-        c.execute('INSERT INTO mytable (action, voice, data) VALUES (?, ?, ?)', (action, voice, data))
+        c.execute('INSERT INTO mytable (action, name, voice, data) VALUES (?, ?, ?, ?)', (action, name, voice, data))
         conn.commit()
         update_listbox()
 
@@ -35,7 +36,7 @@ def add_item():
 root = tk.Tk()
 root.title('SQLite GUI')
 root.resizable(False, False)
-root.geometry('500x560')
+root.geometry('500x650')
 
 # Создание списка и кнопок удаления и добавления
 frame = tk.Frame(root)
@@ -50,14 +51,20 @@ listbox.pack(side=tk.LEFT)
 update_listbox()
 yscrollbar.config(command=listbox.yview)
 xscrollbar.config(command=listbox.xview)
-delete_button = tk.Button(root, text='Delete', command=delete_item, width=10, height=2)
+delete_button = tk.Button(root, text='Удалить', command=delete_item, width=10, height=2,font=('TkDefaultFont', 13))
 delete_button.pack()
 action_var = tk.StringVar(root)
 action_var.set('запусти')
 action_options = ['запусти', 'открой']
+name_var = tk.StringVar(root)
+
 action_menu = tk.OptionMenu(root, action_var, *action_options)
-action_menu.config(width=20, height=2)
+action_menu.config(width=20, height=2,font=('TkDefaultFont', 13))
 action_menu.pack()
+name_label = tk.Label(root, text='Имя:',font=('TkDefaultFont', 13))
+name_label.pack()
+name_entry =  tk.Entry(root,width=35,font=('TkDefaultFont', 13))
+name_entry.pack()
 voice_label = tk.Label(root, text='Что произнести:',font=('TkDefaultFont', 13))
 voice_label.pack()
 voice_entry = tk.Entry(root,width=35,font=('TkDefaultFont', 13))
@@ -66,7 +73,8 @@ data_label = tk.Label(root, text='Данные:',font=('TkDefaultFont', 13))
 data_label.pack()
 data_entry = tk.Entry(root,width=35,font=('TkDefaultFont', 13))
 data_entry.pack()
-add_button = tk.Button(root, text='Add', command=add_item, width=10, height=2)
+
+add_button = tk.Button(root, text='Добавить', command=add_item, width=10, height=2,font=('TkDefaultFont', 13))
 add_button.pack()
 
 # Запуск главного цикла
